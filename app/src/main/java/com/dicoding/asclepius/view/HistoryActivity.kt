26662e7b5ff04ae6e784404b518f837d02e04d24
coro-kunnnnnn <finkazhr@gmail.com>
2025-01-30 22:34:12 +0700

    Layout history

---
+++ app/src/main/java/com/dicoding/asclepius/view/HistoryActivity.kt
@@ -0,0 +1,17 @@
+package com.dicoding.asclepius.view
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.dicoding.asclepius.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_history)
+
+    }
+}
